44dd4f7e9061687e7eae0935bcf5e218e9695d04
Cj <tanedochristian1@gmail.com>
2023-05-10 21:40:08 +0800

    animation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,16 +4,16 @@ const Header = (props: any) => {
   console.log(props);
   return (
     <div
-      className={`flex   h-[8vh] overflow-hidden top-0 left-0 w-full   justify-between px-10 fixed items-center bg-neutral-800 `}
+      className={`flex   h-[8vh] overflow-hidden top-0 left-0 w-full z-50  ${
+        props.color ? "border-b border-gray-200" : ""
+      }   justify-between px-10 fixed items-center bg-neutral-800 `}
       style={{
         backgroundColor: props.color ? "white" : "",
         color: props.color ? "black" : "white",
         transition: "background-color 0.5s ease",
       }}
     >
-      <h1 className="text-2xl font-bold " style={{ fontFamily: "Poppins" }}>
-        I'm Christian
-      </h1>
+      <h1 className="text-2xl font-bold">I'm Christian</h1>
 
       <ul className=" gap-10 font-medium tracking-wide hidden lg:flex">
         <li>About Me</li>
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -9,10 +9,13 @@ import Laravel from "../../img/laravel-icon.png";
 import MysqlIcon from "../../img/mysql.png";
 import MongoIcon from "../../img/mongo.png";
 import ReactTs from "../../img/react-ts.png";
+import JavaIcon from "../../img/java-icon.png";
 const Home = () => {
   const [NavColor, setNavColor] = useState(false);
-
   const [toShow, setToShow] = useState(false);
+
+  const [slideBanner, setSlideBanner] = useState(false);
+  const [slideBanner2, setSlideBanner2] = useState(false);
 
   const changeNavbarColor = () => {
     console.log(window.scrollY);
@@ -20,6 +23,17 @@ const Home = () => {
       setNavColor(true);
     } else {
       setNavColor(false);
+    }
+
+    if (window.scrollY >= 780) {
+      setSlideBanner2(true);
+    } else {
+      setSlideBanner2(false);
+    }
+    if (window.scrollY >= 470) {
+      setSlideBanner(true);
+    } else {
+      setSlideBanner(false);
     }
   };
   window.addEventListener("scroll", changeNavbarColor);
@@ -30,11 +44,10 @@ const Home = () => {
         <Header color={NavColor} />
         <div className="w-full h-full flex gap-2 justify-around ">
           <div className="lg:w-[40%]  w-full flex  justify-center flex-col p-20">
-            <p className="text-xl text-white font-normal">Software Developer</p>
-            <h1
-              className="text-[4rem] text-white font-bold font-[Carme]"
-              style={{ fontFamily: "Carme" }}
-            >
+            <p className="text-xl text-white font-normal carme">
+              Software Developer
+            </p>
+            <h1 className="text-[4rem] text-white font-bold carme">
               Cj Tañedo
             </h1>
           </div>
@@ -45,26 +58,27 @@ const Home = () => {
       </main>
       <section className="w-full h-[50vh] mt-5">
         <div className="w-[full] flex justify-center flex-col gap-3 items-center">
-          <h1
-            className="text-[5rem] font-semibold"
-            style={{ fontFamily: "Carme" }}
-          >
-            Projects
-          </h1>
+          <h1 className="text-[5rem] font-semibold carme">Projects</h1>
 
           <div className="w-full p-2 justify-around mt-20 items-center projects flex flex-wrap gap-10 ">
-            <div className="lg:w-[30%] w-[72%]  flex flex-col gap-2 justify-center ">
+            <div
+              className={`lg:w-[30%] w-[72%]  flex flex-col gap-2 justify-center ${
+                slideBanner ? "w3-animate-left" : ""
+              }`}
+            >
               <h1 className="text-slate-800 text-2xl font-bold">Aruga</h1>
 
-              <p className="text-slate-600 text-[1.1rem] font-normal mt-5 ">
-                Aruga is an online platform marketplace dedicated for parents
-                and babysitter. Find jobs for babysitter and finding babysitter
-                for parent done easily using the Aruga's Matching Algorithm.
+              <p
+                className={`text-slate-600 text-[1.1rem] font-normal mt-5 carme `}
+              >
+                Aruga is an mobile and web marketplace platform dedicated for
+                parents and babysitters. Find jobs for babysitter and finding
+                babysitter for parent done easily.
               </p>
 
               <ul className="flex gap-10 items-center justify-center">
                 <div className="flex w-[200px]">
-                  <img src={Android} className="object-cover" />
+                  <img src={JavaIcon} className="object-cover" />
                 </div>
                 <div className="flex w-[110px]">
                   <img src={ReactIcon} className="object-cover" />
@@ -78,21 +92,26 @@ const Home = () => {
               </ul>
             </div>
             <div className="">
-              <div className="h-[50vh] w-full flex justify-center">
+              <div
+                className={`h-[50vh] w-full flex justify-center  ${
+                  slideBanner ? "w3-animate-right	" : ""
+                }`}
+              >
                 <img src={MobileAruga} className="object-contain" />
               </div>
             </div>
           </div>
 
-          <div className="w-full p-2 justify-around mt-20 items-center projects flex flex-wrap gap-10 flex-row-reverse">
-            <div className="lg:w-[30%] w-[72%]  flex flex-col gap-2 justify-center  ">
-              <h1
-                className="text-slate-800 text-2xl font-bold"
-                style={{ fontFamily: "Carme" }}
-              >
+          <div className="w-full p-2 justify-around mt-20 items-center projects flex flex-wrap flex-row-reverse">
+            <div
+              className={`lg:w-[30%] w-[72%]  flex flex-col gap-2 justify-center  ${
+                slideBanner2 ? "w3-animate-right	" : ""
+              }`}
+            >
+              <h1 className="text-slate-800 text-2xl font-bold carme">
                 Aspect
               </h1>
-              <p className="text-slate-600 text-[1.1rem] font-normal ">
+              <p className="text-slate-600 text-[1.1rem] font-normal carme">
                 Introducing Aruga a mobile and web app, designed to bring the
                 convenience of online shopping to your fingertips. With our app,
                 you can browse through a vast selection of products, from
@@ -100,21 +119,25 @@ const Home = () => {
                 your phone.
               </p>
 
-              <ul className="flex gap-10 items-center justify-center p-3">
-                <div className="flex w-[150px]">
-                  <img src={Android} className="object-cover" />
+              <ul className="flex gap-10 items-center justify-center mt-2">
+                <div className="flex w-[200px]">
+                  <img src={JavaIcon} className="object-cover" />
                 </div>
-                <div className="flex w-[100px]">
+                <div className="flex w-[110px]">
                   <img src={ReactIcon} className="object-cover" />
                 </div>
-                <div className="flex w-[100px]">
+                <div className="flex w-[110px]">
                   <img src={MongoIcon} className="object-cover" />
                 </div>
               </ul>
               <ul className="flex gap-3"></ul>
             </div>
             <div className="">
-              <div className="h-[50vh] w-full flex justify-center">
+              <div
+                className={`h-[50vh] w-full flex justify-center ${
+                  slideBanner2 ? "w3-animate-top" : ""
+                }`}
+              >
                 <img src={AspectAll} className="object-contain" />
               </div>
             </div>
